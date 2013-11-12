1b2f198874bb93e4d110d3dfc746ae23559fc653
diorel <diorelx@gmail.com>
2013-11-12 10:58:46 -0600

    Subir programa  ordenadores a git
    Se agregaron todas las clases y archivos

---
+++ Ordenadores2/Ordenadores2/COrdenador.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ordenadores2
+{
+    class COrdenador
+    {
+        static void Main ( string [] args )
+        {
+        }
+    }
+}
---
+++ Ordenadores2/Ordenadores2/MiOrdenador.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ordenadores2
+{
+    class MiOrdenador
+    {
+        static void Main ( string [] args )
+        {
+            COrdenador miOrdenador = new COrdenador;
+            miOrdenador.Marca = "Alienware";
+            miOrdenador.EncenderOrdenador();
+            miOrdenador.Estado();
+           
+        }
+    }
+}

3407eae5e981d6ec55c050fa818c8a82025c281e
diorel <diorelx@gmail.com>
2013-11-12 11:13:00 -0600

    cambio en antiba
    de actiba a antivada

--- Ordenadores2/Ordenadores2/COrdenador.cs
+++ Ordenadores2/Ordenadores2/COrdenador.cs
@@ -7,8 +7,75 @@ namespace Ordenadores2
 {
     class COrdenador
     {
-        static void Main ( string [] args )
+        // Declaramos las variables
+        private string marca;
+        private string procesador;
+        private int peso;
+        private bool encendido = false;
+        private bool pantalla = false;
+
+        // Declaramos el metodo EncenderOrdenador
+
+        public void EncenderOrdenador ()
         {
+            if ( encendido == true )
+            {
+                System.Console.WriteLine ( "El equipo ya esta encendido" );
+            }
+            else 
+            {
+               encendido = true;
+               pantalla = true;
+               System.Console.WriteLine ("El ha sido encendido");
+            }
         }
+
+        // Se declara el metodo estado en el cual veremos en que estado se encuentra el ordenador
+        public void Estado ()
+        {
+            System.Console.Write ("\nElestado del ordenador es el siguiente");
+            System.Console.Write ( "\nMarca:" + marca );
+            System.Console.Write ( "\nProcesador:" + procesador );
+            System.Console.Write ( "\nPeso:" + peso +"Kg" );
+
+            if ( encendido == true )
+            {
+                System.Console.Write ( "\n El ordenador esta encendido" );
+            }
+
+            else
+            
+                System.Console.Write ( "\n el ordenador esta apagado" );
+            
+
+            if ( pantalla == true )
+            {
+                System.Console.Write ( "\n la pantalla esta activada" );
+            }
+            else
+            
+                System.Console.Write ( "\n la pantalla esta apagada" );
+                System.Console.Write ( "\n" );
+        }
+            
+            
+            // declaramos la propiedad Marca
+
+            public string Marca
+            {
+                get
+                {
+                    return marca;
+                }
+                set
+                {
+                    if(value == null)
+                    
+                        marca = "marca desconocida";
+                    else
+                        marca = value;
+                }
+            }     
+        }      
     }
-}
+
